15e1fb2c399bd5c4a7b8efcc9ac550e08a07b2c2
Mike DeAngelo <deangelo@looker.com>
2018-07-12 15:28:41 -0400

    Added unit tests for role group_rm and role user_rm

--- spec/unit/role/group_rm_spec.rb
+++ spec/unit/role/group_rm_spec.rb
@@ -2,12 +2,32 @@ require 'gzr/commands/role/group_rm'
 
 RSpec.describe Gzr::Commands::Role::GroupRm do
   it "executes `role group_rm` command successfully" do
+    require 'sawyer'
+    groups = (100..105).collect do |i|
+      group_doc = {
+          :id=>i
+      }
+      mock_group = double(Sawyer::Resource, group_doc)
+      allow(mock_group).to receive(:to_attrs).and_return(group_doc)
+      mock_group
+    end
+    mock_sdk = Object.new
+    allow(mock_sdk).to receive(:logout)
+    allow(mock_sdk).to receive(:role_groups) do |role_id,body|
+      groups
+    end
+    allow(mock_sdk).to receive(:set_role_groups) do |role_id,body|
+      expect(body).to contain_exactly(100, 101, 102, 104, 105)
+      nil 
+    end
     output = StringIO.new
     options = {}
-    command = Gzr::Commands::Role::GroupRm.new(options)
+    command = Gzr::Commands::Role::GroupRm.new(1, [103], options)
+
+    command.instance_variable_set(:@sdk, mock_sdk)
 
     command.execute(output: output)
 
-    expect(output.string).to eq("OK\n")
+    expect(output.string).to eq("")
   end
 end
--- spec/unit/role/user_rm_spec.rb
+++ spec/unit/role/user_rm_spec.rb
@@ -2,12 +2,32 @@ require 'gzr/commands/role/user_rm'
 
 RSpec.describe Gzr::Commands::Role::UserRm do
   it "executes `role user_rm` command successfully" do
+    require 'sawyer'
+    users = (100..105).collect do |i|
+      user_doc = {
+          :id=>i
+      }
+      mock_user = double(Sawyer::Resource, user_doc)
+      allow(mock_user).to receive(:to_attrs).and_return(user_doc)
+      mock_user
+    end
+    mock_sdk = Object.new
+    allow(mock_sdk).to receive(:logout)
+    allow(mock_sdk).to receive(:role_users) do |role_id,body|
+      users
+    end
+    allow(mock_sdk).to receive(:set_role_users) do |role_id,body|
+      expect(body).to contain_exactly(100, 101, 102, 104, 105)
+      nil 
+    end
     output = StringIO.new
     options = {}
-    command = Gzr::Commands::Role::UserRm.new(options)
+    command = Gzr::Commands::Role::UserRm.new(1,[103],options)
+
+    command.instance_variable_set(:@sdk, mock_sdk)
 
     command.execute(output: output)
 
-    expect(output.string).to eq("OK\n")
+    expect(output.string).to eq("")
   end
 end
