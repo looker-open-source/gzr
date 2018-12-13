99c1eadf982026bab708b1b96c5f9ed156c2b7e3
Mike DeAngelo <deangelo@looker.com>
2018-12-13 17:58:24 -0500

    fix frozen_string_literal directive

--- lib/gzr/commands/space/top.rb
+++ lib/gzr/commands/space/top.rb
@@ -19,7 +19,7 @@
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-frozen_string_literal: true
+# frozen_string_literal: true
 
 require_relative '../../command'
 require_relative '../../modules/space'
