8616180838f6dc3df680cc8fefd60f61ed8a58f1
Mike DeAngelo <drstrangelove@google.com>
2021-10-29 13:11:24 -0400

    sample of a test against a live server

---
+++ spec/live/user/me_spec.rb
@@ -0,0 +1,33 @@
+# The MIT License (MIT)
+
+# Copyright (c) 2018 Mike DeAngelo Looker Data Sciences, Inc.
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+RSpec.describe "`gzr user me` command", type: :cli do
+  it "executes `user me` command successfully against a live server" do
+    output = `gzr user me`
+    expect(output).to eq <<-OUT
++--+------------------------+---------+----------+-----------------+-------------+
+|id|email                   |last_name|first_name|personal_space_id|home_space_id|
++--+------------------------+---------+----------+-----------------+-------------+
+| 1|firstadmin@schmoogle.com|Rekool   |Firstadmin|                 |             |
++--+------------------------+---------+----------+-----------------+-------------+
+    OUT
+  end
+end
