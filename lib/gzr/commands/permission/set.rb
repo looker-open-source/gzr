1f70cb44a9d54c457b1552396782161528b7e3e9
Mike DeAngelo <drstrangelove@google.com>
2023-05-16 12:13:35 -0400

    feat: cat, import, rm permission sets

--- lib/gzr/commands/permission/set.rb
+++ lib/gzr/commands/permission/set.rb
@@ -48,6 +48,50 @@ module Gzr
           end
         end
 
+        desc 'cat PERMISSION_SET_ID', 'Output json information about a permission set to screen or file'
+        method_option :help, aliases: '-h', type: :boolean,
+                             desc: 'Display usage information'
+        method_option :dir,  type: :string,
+                             desc: 'Directory to store output file'
+        method_option :trim, type: :boolean,
+                             desc: 'Trim output to minimal set of fields for later import'
+        def cat(permission_set_id)
+          if options[:help]
+            invoke :help, ['cat']
+          else
+            require_relative 'set/cat'
+            Gzr::Commands::Permission::Set::Cat.new(permission_set_id,options).execute
+          end
+        end
+
+        desc 'import FILE', 'Import a permission set from a file'
+        method_option :help, aliases: '-h', type: :boolean,
+                             desc: 'Display usage information'
+        method_option :force, type: :boolean,
+                             desc: 'Overwrite an existing permission set'
+        method_option :plain, type: :boolean, default: false,
+                             desc: 'print without any extra formatting'
+        def import(file)
+          if options[:help]
+            invoke :help, ['import']
+          else
+            require_relative 'set/import'
+            Gzr::Commands::Permission::Set::Import.new(file, options).execute
+          end
+        end
+
+        desc 'rm PERMISSION_SET_ID', 'Delete the permission_set given by PERMISSION_SET_ID'
+        method_option :help, aliases: '-h', type: :boolean,
+                             desc: 'Display usage information'
+        def rm(permission_set_id)
+          if options[:help]
+            invoke :help, ['delete']
+          else
+            require_relative 'set/rm'
+            Gzr::Commands::Permission::Set::Delete.new(permission_set_id,options).execute
+          end
+        end
+
       end
     end
   end
--- lib/gzr/modules/permission/set.rb
+++ lib/gzr/modules/permission/set.rb
@@ -39,6 +39,85 @@ module Gzr
         end
       end
 
+      def cat_permission_set(set_id, fields=nil)
+        req = {}
+        req[:fields] = fields if fields
+        begin
+          return @sdk.permission_set(set_id,req)&.to_attrs
+        rescue LookerSDK::NotFound => e
+          return nil
+        rescue LookerSDK::Error => e
+          say_error "Error querying permission_set(#{set_id},#{JSON.pretty_generate(req)})"
+          say_error e
+          raise
+        end
+      end
+
+      def trim_permission_set(data)
+        trimmed = data.select do |k,v|
+          (keys_to_keep('update_permission_set') + [:id,:built_in]).include? k
+        end
+        trimmed
+      end
+
+      def search_permission_sets(fields: nil, sorts: nil, id: nil, name: nil, all_access: nil, built_in: nil, filter_or: nil)
+        data = []
+        begin
+          req = {}
+          req[:fields] = fields unless fields.nil?
+          req[:sorts] = sorts unless sorts.nil?
+          req[:id] = id unless id.nil?
+          req[:name] = name unless name.nil?
+          req[:all_access] = all_access unless all_access.nil?
+          req[:built_in] = built_in unless built_in.nil?
+          req[:filter_or] = filter_or unless filter_or.nil?
+          req[:limit] = 64
+          loop do
+            page = @sdk.search_permission_sets(req)
+            data+=page
+            break unless page.length == req[:limit]
+            req[:offset] = (req[:offset] || 0) + req[:limit]
+          end
+        rescue LookerSDK::NotFound => e
+          # do nothing
+        rescue LookerSDK::Error => e
+          say_error "Error calling search_permission_sets(#{JSON.pretty_generate(req)})"
+          say_error e
+          raise
+        end
+        data
+      end
+
+      def update_permission_set(permission_set_id, data)
+        begin
+          return @sdk.update_permission_set(permission_set_id, data)&.to_attrs
+        rescue LookerSDK::Error => e
+          say_error "Error calling update_permission_set(#{permission_set_id},#{JSON.pretty_generate(data)})"
+          say_error e
+          raise
+        end
+      end
+
+      def create_permission_set(data)
+        begin
+          return @sdk.create_permission_set(data)&.to_attrs
+        rescue LookerSDK::Error => e
+          say_error "Error calling create_permission_set(#{JSON.pretty_generate(data)})"
+          say_error e
+          raise
+        end
+      end
+
+      def delete_permission_set(permission_set_id)
+        begin
+          return @sdk.delete_permission_set(permission_set_id)
+        rescue LookerSDK::Error => e
+          say_error "Error calling delete_permission_set(#{permission_set_id}})"
+          say_error e
+          raise
+        end
+      end
+
     end
   end
 end
