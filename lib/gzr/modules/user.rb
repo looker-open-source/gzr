b712a59f21c578fd654dcfc96bf2378d72b961c4
Mike DeAngelo <drstrangelove@google.com>
2023-08-30 12:53:17 -0400

    fix: tech debt - removed deprecated paging in users

--- lib/gzr/modules/user.rb
+++ lib/gzr/modules/user.rb
@@ -38,7 +38,9 @@ module Gzr
       begin
         @sdk.user(id, fields ? {:fields=>fields} : nil ).to_attrs
       rescue LookerSDK::NotFound => e
-        nil
+        say_error "user(#{id}) not found"
+        say_error e
+        raise
       rescue LookerSDK::Error => e
         say_error "Error querying user(#{id},{:fields=>\"#{fields}\"})"
         say_error e
@@ -48,55 +50,51 @@ module Gzr
 
     def search_users(filter, fields=nil, sorts=nil)
       req = {
-        :per_page=>128
+        :limit=>64
       }
       req.merge!(filter)
       req[:fields] = fields if fields
       req[:sorts] = sorts if sorts
 
       data = Array.new
-      page = 1
-      loop do
-        begin
-          req[:page] = page
-          scratch_data = @sdk.search_users(req).collect { |r| r.to_attrs }
-        rescue LookerSDK::NotFound => e
-          nil
-        rescue LookerSDK::ClientError => e
-          say_error "Unable to get search_users(#{JSON.pretty_generate(req)})"
-          say_error e
-          raise
+      begin
+        loop do
+          page = @sdk.search_users(req).collect { |r| r.to_attrs }
+          data+=page
+          break unless page.length == req[:limit]
+          req[:offset] = (req[:offset] || 0) + req[:limit]
         end
-        break if scratch_data and scratch_data.length == 0
-        page += 1
-        data += scratch_data
+      rescue LookerSDK::NotFound => e
+        # do nothing
+      rescue LookerSDK::ClientError => e
+        say_error "Unable to get search_users(#{JSON.pretty_generate(req)})"
+        say_error e
+        raise
       end
       data
     end
 
     def query_all_users(fields=nil, sorts=nil)
       req = {
-        :per_page=>128
+        :limit=>64
       }
       req[:fields] = fields if fields
       req[:sorts] = sorts if sorts
 
       data = Array.new
-      page = 1
-      loop do
-        begin
-          req[:page] = page
-          scratch_data = @sdk.all_users(req).collect { |r| r.to_attrs }
-        rescue LookerSDK::NotFound => e
-          nil
-        rescue LookerSDK::ClientError => e
-          say_error "Unable to get all_users(#{JSON.pretty_generate(req)})"
-          say_error e
-          raise
+      begin
+        loop do
+          page = @sdk.all_users(req).collect { |r| r.to_attrs }
+          data+=page
+          break unless page.length == req[:limit]
+          req[:offset] = (req[:offset] || 0) + req[:limit]
         end
-        break if scratch_data and scratch_data.length == 0
-        page += 1
-        data += scratch_data
+      rescue LookerSDK::NotFound => e
+        # do nothing
+      rescue LookerSDK::ClientError => e
+        say_error "Unable to get all_users(#{JSON.pretty_generate(req)})"
+        say_error e
+        raise
       end
       data
     end
@@ -105,7 +103,9 @@ module Gzr
       begin
         @sdk.update_user(id,req)&.to_attrs
       rescue LookerSDK::NotFound => e
-        nil
+        say_error "updating user(#{id},#{JSON.pretty_generate(req)}) not found"
+        say_error e
+        raise
       rescue LookerSDK::Error => e
         say_error "Error updating user(#{id},#{JSON.pretty_generate(req)})"
         say_error e
@@ -115,9 +115,11 @@ module Gzr
 
     def delete_user(id)
       begin
-        @sdk.delete_user(id)&.to_attrs
+        @sdk.delete_user(id)
       rescue LookerSDK::NotFound => e
-        nil
+        say_error "delete user(#{id},#{JSON.pretty_generate(req)}) not found"
+        say_error e
+        raise
       rescue LookerSDK::Error => e
         say_error "Error deleting user(#{id})"
         say_error e
