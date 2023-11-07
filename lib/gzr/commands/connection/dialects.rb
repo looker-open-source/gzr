1a45294a4e97f714c7d523d8bf6e8a01c5341380
Mike DeAngelo <drstrangelove@google.com>
2023-11-07 14:40:08 -0500

    fix: remove eval from lib/gzr/commands/connection/dialects.rb

--- lib/gzr/commands/connection/dialects.rb
+++ lib/gzr/commands/connection/dialects.rb
@@ -47,11 +47,8 @@ module Gzr
             table_hash = Hash.new
             fields = field_names(@options[:fields])
             table_hash[:header] = data[0].keys unless @options[:plain]
-            expressions = fields.collect { |fn| field_expression_hash(fn) }
             table_hash[:rows] = data.map do |row|
-              expressions.collect do |e|
-                eval "row#{e}"
-              end
+              field_expressions_eval(fields,row)
             end
             table = TTY::Table.new(table_hash)
             alignments = fields.collect do |k|
